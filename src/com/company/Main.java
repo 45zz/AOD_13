59d573cada4b5b6b98d8e36f0f7de3d4948742d1
45zz <bugro.home@gmail.com>
2021-01-19 17:16:52 +0300

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,18 @@ public class Main {
         Graph graph=new Graph();
         System.out.println(graph.kruskala());
         graph.showKrustal();
+//         7 12
+//        1 2 20
+//        2 3 15
+//        3 4 3
+//        4 5 17
+//        5 6 28
+//        6 7 36
+//        1 7 1
+//        2 7 4
+//        7 3 9
+//        7 4 16
+//        7 5 25
+//        6 1 23
     }
 }
